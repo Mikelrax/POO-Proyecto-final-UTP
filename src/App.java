645fea6571b82b60b6e38c870b8ad972f2ad1037
Daniel <danny2003renato@gmail.com>
2025-06-29 23:20:54 -0500

    refactor: simplify main application entry point by using MainFrame

--- src/App.java
+++ src/App.java
@@ -1,25 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
-import java.net.URL;
-
+import javax.swing.SwingUtilities;
+import views.MainFrame;
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("First Screen");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(400, 300);
-            URL iconURL = App.class.getResource("assets/logo.png");
-            if (iconURL != null) {
-                ImageIcon icon = new ImageIcon(iconURL);
-                frame.setIconImage(icon.getImage());
-            } else {
-                System.out.println("No se encontró el archivo logo.png");
-            }
-
-            JLabel label = new JLabel("Hello World!", SwingConstants.CENTER);
-            frame.add(label);
-            frame.setTitle("Programación orientada a objetos");
-            frame.setVisible(true);
+            new MainFrame().setVisible(true);
         });
     }
 }
